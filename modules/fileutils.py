08056ed51e59ae4c855bbeb84ffa882dc1b8e5da
João Pedro Neto <jpneto@fc.ul.pt>
2020-06-22 14:12:03 +0100

    Update fileutils.py

--- modules/fileutils.py
+++ modules/fileutils.py
@@ -11,13 +11,3 @@ def countLines(filename):
 def countLinesBy(filename, p):
     f = open(filename, 'r')
     return len([line for line in f.readlines() if p(line)])
-
-
-# use eg:
-
-# for line in fileMatch('data/days.txt', 'sd'):
-#     print(line.rstrip())
-    
-# print(countLines('data/days.txt'))
-
-# print(countLinesBy('data/days.txt', lambda line : len(line)<=7))
